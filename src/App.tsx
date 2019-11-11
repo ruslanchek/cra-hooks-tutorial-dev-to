96f3a3cd3790e014ed5bffaf2d368e9f378f6906
Ruslan Shashkov <ruslanchek@gmail.com>
2019-11-11 21:38:51 +0200

    App with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+const HomePage = () => (
+  <div>
+    <h1>Home</h1>
+    <p>Welcome!</p>
+  </div>
+);
+
+const PublicPage = () => (
+  <div>
+    <h1>Public page</h1>
+    <p>Nothing special here</p>
+  </div>
+);
+
+const PrivatePage = () => (
+  <div>
+    <h1>Private page</h1>
+    <p>Wake up, Neo...</p>
+  </div>
+);
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/public'>Public</Link>
+        </li>
+        <li>
+          <Link to='/private'>Private</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/public' component={PublicPage} />
+        <Route exact path='/private' component={PrivatePage} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
